dcc878dd779db4907c053fd98ffc58ac87572557
Tatchan <apple@TatchannoMacBook-Air.local>
2024-02-26 16:00:11 +0900

    travel_plans→tripsにモデル名変更

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,32 @@
+class TripsController < ApplicationController
+  def index
+    @trips = Trip.where(group_id: params[:group_id])
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.group_id = (params[:group_id])
+    if @trip.save
+      redirect_to group_trips_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title)
+  end
+end
