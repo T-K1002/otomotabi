85a06c6b6609440bea6ef8950bfcfb168ab141ca
Tatchan <apple@TatchannoMacBook-Air.local>
2024-03-08 14:01:55 +0900

    feat: #39 グループ申請一覧をオーナーのみが見れるように変更

--- app/controllers/permits_controller.rb
+++ app/controllers/permits_controller.rb
@@ -1,4 +1,7 @@
 class PermitsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:create, :destroy, :index]
+
   def create
     @group = Group.find(params[:group_id])
     permit = current_user.permits.new(group_id: params[:group_id])
@@ -14,5 +17,14 @@ class PermitsController < ApplicationController
 
   def index
     @permits = Permit.where(group_id: params[:group_id])
+  end
+
+  private
+
+  def ensure_correct_user
+    @group = Group.find(params[:group_id])
+    unless @group.owner_id == current_user.id
+      redirect_to group_path(@group), alert: "グループオーナーのみ編集が可能です"
+    end
   end
 end
