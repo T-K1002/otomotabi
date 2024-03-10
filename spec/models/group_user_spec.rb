0e08d7be642e1db3fba9af7d14941a675223ed66
Tatchan <apple@TatchannoMacBook-Air.local>
2024-03-10 17:38:03 +0900

    test: #44 モデルスペックの作成

--- spec/models/group_user_spec.rb
+++ spec/models/group_user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe GroupUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#group_user" do
+  let!(:group_user) { create(:group_user) }
+
+    it "ユーザーID、グループIDがある場合、有効である" do
+      expect(group_user).to be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#user" do
+  let!(:user) { create(:user) }
+
+    it "メールアドレス、パスワードがある場合、有効である" do
+      expect(user).to be_valid
+    end
+  end
 end
