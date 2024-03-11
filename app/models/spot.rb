d6a63ce5ebeae553f4adfc409b2543efe0ec4f12
Tatchan <apple@TatchannoMacBook-Air.local>
2024-03-11 22:07:53 +0900

    feat: #46 バリデーションの記述を追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :trip
+
+  validates :name, presence: true
+  validates :start_time, presence: true
+  validates :end_time, comparison: { greater_than: :start_time }
 end
