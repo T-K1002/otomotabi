dde09b2bc4a86e8638c354400655c7aa65385050
Tatchan <apple@TatchannoMacBook-Air.local>
2024-03-12 22:48:28 +0900

    test: #48 バリデーションに関するテストを追加

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,8 +4,20 @@ RSpec.describe Group, type: :model do
   describe "#group" do
   let!(:group) { create(:group) }
 
-    it "タイトル、紹介文がある場合、有効である" do
+    it "グループ名、紹介文がある場合、有効である" do
       expect(group).to be_valid
+    end
+
+    it "グループ名がない場合はエラーメッセージを表示" do
+      group.name = nil
+      group.valid?
+      expect(group.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "紹介文がない場合はエラーメッセージを表示" do
+      group.introduction = nil
+      group.valid?
+      expect(group.errors.full_messages).to include("Introduction can't be blank")
     end
   end
 end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -7,5 +7,31 @@ RSpec.describe Spot, type: :model do
     it "スポット名、旅行日、開始時間、終了時間がある場合、有効である" do
       expect(spot).to be_valid
     end
+
+    it "スポット名がない場合はエラーメッセージを表示" do
+      spot.name = nil
+      spot.valid?
+      expect(spot.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "旅行日がない場合はエラーメッセージを表示" do
+      spot.date = nil
+      spot.valid?
+      expect(spot.errors.full_messages).to include("Date can't be blank")
+    end
+
+    it "開始時間がない場合はエラーメッセージを表示" do
+      spot.start_time = nil
+      spot.valid?
+      expect(spot.errors.full_messages).to include("Start time can't be blank")
+    end
+
+    it "終了時間がない場合はエラーメッセージを表示" do
+      spot.end_time = nil
+      spot.valid?
+      expect(spot.errors.full_messages).to include("End time can't be blank")
+    end
+
+
   end
 end
