5c6d0b9953cb2c1b84105b1a9980c8e5c0fd3e51
Tatchan <apple@TatchannoMacBook-Air.local>
2024-03-19 23:02:56 +0900

    google map apiを使用しグーグルマップを表示

---
+++ app/javascript/googlemap.js
@@ -0,0 +1,43 @@
+let map;
+let markerLatLng;
+let marker = []; 
+let infoWindow = [];
+const markerData = gon.spots; 
+const markerDataLat = gon.spots_latitudes;
+const markerDataLng = gon.spots_longitudes;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), { maxZoom: 15,});
+  map.fitBounds(new google.maps.LatLngBounds(
+    {
+      lat: Math.min(...markerDataLat),
+      lng: Math.min(...markerDataLng),
+    },
+    {
+      lat: Math.max(...markerDataLat),
+      lng: Math.max(...markerDataLng),
+    }
+  ));
+
+  for (let i = 0; i < markerData.length; i++) {
+    markerLatLng = new google.maps.LatLng({lat: markerData[i].latitude, lng: markerData[i].longitude}); 
+    marker[i] = new google.maps.Marker({
+      map: map,
+      position: markerLatLng,
+    });
+
+    infoWindow[i] = new google.maps.InfoWindow({ 
+    content: '<div class="info">' + markerData[i].name + '</div>' 
+    });
+
+    markerEvent(i);
+  }
+}
+
+function markerEvent(i) {
+  marker[i].addListener('click', function() { 
+    infoWindow[i].open(map, marker[i]); 
+  });
+}
+
+window.initMap = initMap
