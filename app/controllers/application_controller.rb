05ae334599b5c4a40bea1047fd928dc3adf58567
Tatchan <apple@TatchannoMacBook-Air.local>
2024-03-27 15:23:18 +0900

    feat: #58 ログインユーザーのグループを取得

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :get_group_user
+  def get_group_user
+    @groups = Group.includes(:users).where(users: { id: current_user.id })
+  end
 end
