07340adc03376b45a71513e4909b6455ea95b5f6
Tatchan <apple@TatchannoMacBook-Air.local>
2024-03-28 16:19:06 +0900

    feat: #60 ストロングパラメータの追加--

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :get_group_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def get_group_user
-    @groups = Group.includes(:users).where(users: { id: current_user.id })
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @groups = Group.includes(:users).where(users: { id: current_user.id })
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
 end
