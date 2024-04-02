c5a5bc031b758c6028fd5a19ffd3b1dcc54197f1
Tatchan <apple@TatchannoMacBook-Air.local>
2024-04-02 11:27:56 +0900

    feat: #64 openaiのクラスメソッドを作成

---
+++ app/models/open_ai.rb
@@ -0,0 +1,14 @@
+class OpenAi
+  require 'ruby/openai'
+
+  def self.generate_recommend(prefecture, genre, spots)
+    client = OpenAI::Client.new(access_token: ENV["OPEN_AI_API_KEY"])
+    response = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: "#{prefecture}で#{genre}が有名な、#{spots}以外のおすすめの旅行場所とその理由を教えてください" }],
+      }
+    )
+    response.dig('choices', 0, 'message', 'content')
+  end
+end
