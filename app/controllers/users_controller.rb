78508f7da7a51e74dc3df618ebead4ffe44831d6
Tatchan <apple@TatchannoMacBook-Air.local>
2024-04-08 10:14:26 +0900

    feat: #68 ユーザーページにグループ一覧を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
+    @groups = Group.includes(:users).where(users: { id: current_user.id })
+    @requests = Permit.where(user_id: current_user.id)
     if @user != current_user
       redirect_to user_path(current_user), alert: "不正なアクセスです。"
     end
