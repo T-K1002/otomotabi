1b655f7b4edb3016f6975e2d08abdb09158a8cb4
Tatchan <apple@TatchannoMacBook-Air.local>
2024-04-20 13:51:13 +0900

    feat: #82 ゲストログイン機能実装のため、ルーディングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  devise_for :users
-  resources :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_scope :user do
+    post "users/guest_sign_in" => "users/sessions#guest_sign_in"
+  end
+  resources :users, only: [:index, :show]
   resources :groups do
     resources :permits, only: [:index, :create, :destroy]
     resources :group_users, only: [:create, :destroy]
