e4aa62ef57c62befc344f71b428c2a145e767c30
Tatchan <apple@TatchannoMacBook-Air.local>
2024-04-26 12:04:36 +0900

    feat: #86 新規作成、アカウント編集後のリダイレクト先を変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: [:destroy, :update]
-
   def ensure_normal_user
-    if resource.email == "guest@example.com"
-      redirect_to root_path, alert: "ゲストユーザーは更新・削除できません。"
+    if resource.guest == true
+      redirect_to groups_path, alert: "ゲストユーザーは更新・削除できません。"
     end
   end
+
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "新規登録に成功しました。"
+    groups_path
+  end
+
+  def after_update_path_for(resource)
+    flash[:notice] = "アカウントの編集に成功しました。"
+    user_path(resource)
+   end
 end
