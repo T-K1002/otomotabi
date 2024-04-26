178e41a323356cdc40710e7f445d378cfe6745b9
Tatchan <apple@TatchannoMacBook-Air.local>
2024-04-26 12:34:10 +0900

    feat: #86 ゲストログインをアカウント独立型に変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,20 +1,20 @@
 class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     if current_user
-      redirect_to groups_path, alert: "すでにログインしています." 
+      redirect_to groups_path, alert: "すでにログインしています。"
     else
       guest_user = User.guest
       guest_group = Group.guest_group(guest_user)
-      guest_group_user = GroupUser.guest_group_user(guest_user, guest_group)
+      GroupUser.guest_group_user(guest_user, guest_group)
       guest_trip = Trip.guest_trip(guest_group)
-      guest_spot = Spot.guest_spot(guest_group, guest_trip)
+      Spot.guest_spot(guest_group, guest_trip)
       sign_in guest_user
-      redirect_to groups_path, notice: "ゲストユーザーとしてログインしました"
+      redirect_to groups_path, notice: "ゲストユーザーとしてログインしました。"
     end
   end
 
   def after_sign_in_path_for(resource)
-    flash[:notice] = "ログインに成功しました"
+    flash[:notice] = "ログインに成功しました。"
     groups_path
   end
 end
