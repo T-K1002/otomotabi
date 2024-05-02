321e6cb5daf832e9a7b45a9c533703ee7a78fe99
Tatchan <apple@TatchannoMacBook-Air.local>
2024-05-02 12:55:45 +0900

    test: #90 テストデータにmemoカラム追加

--- spec/factories/spots.rb
+++ spec/factories/spots.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :spot do
-    name { Faker::Lorem.sentence }
+    memo { Faker::Lorem.sentence(word_count: 3) }
     date { "2024/2/3" } 
     start_time { "7:00" }
     end_time { "8:00" }
