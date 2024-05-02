584c8de96181b319d2af6c3748c933aea67f618a
Tatchan <apple@TatchannoMacBook-Air.local>
2024-05-02 12:48:26 +0900

    test: #90 紹介文の文字数を制限

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :group do
     name { Faker::Name.name }
-    introduction { Faker::Lorem.sentence }
+    introduction { Faker::Lorem.sentence(word_count: 1) }
 
     association :owner
   end
