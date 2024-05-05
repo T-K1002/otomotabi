9ea303268f87de77f11b553e350627f36699e82e
Tatchan <apple@TatchannoMacBook-Air.local>
2024-05-05 20:12:00 +0900

    feat: #98 おすすめの旅行場所を3つ取得できるように変更

--- app/models/open_ai.rb
+++ app/models/open_ai.rb
@@ -6,7 +6,7 @@ class OpenAi
     response = client.chat(
       parameters: {
         model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: "#{spots}を除く、#{prefecture}で#{genre}が有名な旅行場所とその理由を教えてください" }],
+        messages: [{ role: 'user', content: "#{spots}以外の、#{prefecture}で#{genre}が有名な旅行場所とその理由を3つ教えてください" }],
       }
     )
     response.dig('choices', 0, 'message', 'content')
